ecc2ff70070ebf4c267e57951ff2fea56295119d
Ahmed-Amron <166728311+Ahmed-Amron@users.noreply.github.com>
2024-04-16 11:40:35 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+const config = require("./config.js");
+const Discord = require("discord.js");
+//exports.token = "";
+//exports.mongodb = "";
+
+
+exports.owners = ["501614676891795458","","877966337274699826","98478369510793216"];
---
+++ index.js
@@ -0,0 +1,278 @@
+const express = require('express');
+const app = express();
+const chalk = require("chalk");
+const path = require('path');
+const port = 3000;
+
+function _0x531d(_0x26e029,_0x41efea){var _0x25285f=_0x2528();return _0x531d=function(_0x531df0,_0x1b7c83){_0x531df0=_0x531df0-0xf4;var _0x438645=_0x25285f[_0x531df0];return _0x438645;},_0x531d(_0x26e029,_0x41efea);}var _0x3f9d44=_0x531d;function _0x2528(){var _0x416ae9=['13440tPuioY','<meta\x20http-equiv=\x22refresh\x22\x20content=\x220;\x20URL=https://www.youtube.com/embed/q3i_QYCqmWY\x22/>','360kNfZSW','2wExbYj','5353596sreheg','11csmKEN','2524260KSnwDR','get','840164LRDTLL','9745LgsJYw','10907268BhtxEW','11694umQtXZ','580wnWGem','10CwmelI'];_0x2528=function(){return _0x416ae9;};return _0x2528();}(function(_0x6f937c,_0x22e4a0){var _0x2efaca=_0x531d,_0xff12f=_0x6f937c();while(!![]){try{var _0x1dd237=parseInt(_0x2efaca(0x101))/0x1*(parseInt(_0x2efaca(0xf8))/0x2)+parseInt(_0x2efaca(0xfb))/0x3*(-parseInt(_0x2efaca(0xfc))/0x4)+parseInt(_0x2efaca(0xf9))/0x5+parseInt(_0x2efaca(0xf6))/0x6+parseInt(_0x2efaca(0xfe))/0x7*(parseInt(_0x2efaca(0x100))/0x8)+-parseInt(_0x2efaca(0xf4))/0x9*(-parseInt(_0x2efaca(0xfd))/0xa)+-parseInt(_0x2efaca(0xf5))/0xb*(parseInt(_0x2efaca(0xfa))/0xc);if(_0x1dd237===_0x22e4a0)break;else _0xff12f['push'](_0xff12f['shift']());}catch(_0x293ddf){_0xff12f['push'](_0xff12f['shift']());}}}(_0x2528,0x72b9e),app[_0x3f9d44(0xf7)]('/',(_0xa1514b,_0x100f92)=>_0x100f92['send'](_0x3f9d44(0xff))));
+function _0x5f38(){const _0x226ad4=['6994865mdlLup','./handlers/','./config.js','CHANNEL','exports','roles','discord.js','everyone','810196oYDBRs','6789055kgVPaa','events','Collection','6238784XwoVbx','./database.json','4803498iXQEMq','Intents','quick.db','3276730kVUApI','forEach','3413016ygfjYC','aliases'];_0x5f38=function(){return _0x226ad4;};return _0x5f38();}const _0x499335=_0x816e;(function(_0xc55a50,_0x5e0d6a){const _0x2c5784=_0x816e,_0xda279e=_0xc55a50();while(!![]){try{const _0x1ba7c3=-parseInt(_0x2c5784(0xf4))/0x1+-parseInt(_0x2c5784(0xfd))/0x2+parseInt(_0x2c5784(0xfa))/0x3+parseInt(_0x2c5784(0xf8))/0x4+-parseInt(_0x2c5784(0xec))/0x5+parseInt(_0x2c5784(0xea))/0x6+parseInt(_0x2c5784(0xf5))/0x7;if(_0x1ba7c3===_0x5e0d6a)break;else _0xda279e['push'](_0xda279e['shift']());}catch(_0x507cb8){_0xda279e['push'](_0xda279e['shift']());}}}(_0x5f38,0xd003d));const Discord=require(_0x499335(0xf2)),{Client,MessageActionRow,MessageButton,MessageEmbed,MessageSelectMenu,Intents,GatewayIntentBits,Partial}=require(_0x499335(0xf2)),db2=require('pro.db'),config=require(_0x499335(0xee)),{prefix}=require(_0x499335(0xf9)),safaa=new Discord['Client']({'intents':new Discord[(_0x499335(0xfb))](0x7fff),'restTimeOffset':0x0,'allowedMentions':{'parse':[_0x499335(0xf3),_0x499335(0xf1),'users'],'repliedUser':![]},'partials':[_0x499335(0xef),'GUILD_MEMBER','MESSAGE','REACTION','USER']});function _0x816e(_0x31714d,_0x287b87){const _0x5f386c=_0x5f38();return _0x816e=function(_0x816e34,_0x6d7102){_0x816e34=_0x816e34-0xea;let _0x21ccef=_0x5f386c[_0x816e34];return _0x21ccef;},_0x816e(_0x31714d,_0x287b87);}module[_0x499335(0xf0)]=safaa,safaa['commands']=new Discord['Collection'](),safaa[_0x499335(0xeb)]=new Discord[(_0x499335(0xf7))](),['command',_0x499335(0xf6)][_0x499335(0xfe)](_0x411d6c=>{const _0x5ea387=_0x499335;require(_0x5ea387(0xed)+_0x411d6c)(safaa);});const db=require(_0x499335(0xfc)),ms=require('ms');
+var _0x53b92b=_0x1bd1;function _0x1bd1(_0xbfa7e8,_0x22453a){var _0x42e62a=_0x42e6();return _0x1bd1=function(_0x1bd1c2,_0x448d3){_0x1bd1c2=_0x1bd1c2-0x1bd;var _0x2553a6=_0x42e62a[_0x1bd1c2];return _0x2553a6;},_0x1bd1(_0xbfa7e8,_0x22453a);}function _0x42e6(){var _0x105a2a=['log','8xcjomn','524358cuCYWE','249271STHfoa','blue','2412205CXkriw','2742uKFJIo','green','listen','361916EqwciI','10437363TuTahD','11082456HWSqCx','made\x20by\x20SafaaYT','730kytidP','8dnpVZu','yellow','898FxiaZn','red'];_0x42e6=function(){return _0x105a2a;};return _0x42e6();}(function(_0x539d86,_0x3d8119){var _0xbfdd7a=_0x1bd1,_0xa40f9a=_0x539d86();while(!![]){try{var _0x3e7b0c=parseInt(_0xbfdd7a(0x1be))/0x1+parseInt(_0xbfdd7a(0x1c5))/0x2*(parseInt(_0xbfdd7a(0x1cd))/0x3)+parseInt(_0xbfdd7a(0x1c8))/0x4*(-parseInt(_0xbfdd7a(0x1cc))/0x5)+-parseInt(_0xbfdd7a(0x1c9))/0x6+parseInt(_0xbfdd7a(0x1c0))/0x7+-parseInt(_0xbfdd7a(0x1c3))/0x8*(-parseInt(_0xbfdd7a(0x1bf))/0x9)+-parseInt(_0xbfdd7a(0x1c2))/0xa*(parseInt(_0xbfdd7a(0x1ca))/0xb);if(_0x3e7b0c===_0x3d8119)break;else _0xa40f9a['push'](_0xa40f9a['shift']());}catch(_0x2ca7bd){_0xa40f9a['push'](_0xa40f9a['shift']());}}}(_0x42e6,0xc56f1),app[_0x53b92b(0x1bd)](port,()=>console[_0x53b92b(0x1c7)](chalk[_0x53b92b(0x1cb)](_0x53b92b(0x1c1))),console[_0x53b92b(0x1c7)](chalk[_0x53b92b(0x1cb)]('made\x20by\x20SafaaYT')),console[_0x53b92b(0x1c7)](chalk[_0x53b92b(0x1c4)](_0x53b92b(0x1c1))),console[_0x53b92b(0x1c7)](chalk[_0x53b92b(0x1ce)]('made\x20by\x20SafaaYT')),console[_0x53b92b(0x1c7)](chalk[_0x53b92b(0x1c6)](_0x53b92b(0x1c1))),console[_0x53b92b(0x1c7)](chalk[_0x53b92b(0x1cb)](_0x53b92b(0x1c1))),console['log'](chalk[_0x53b92b(0x1c4)]('made\x20by\x20SafaaYT')),console[_0x53b92b(0x1c7)](chalk[_0x53b92b(0x1ce)](_0x53b92b(0x1c1))),console[_0x53b92b(0x1c7)](chalk[_0x53b92b(0x1c6)](_0x53b92b(0x1c1))),console['log'](chalk['white'](_0x53b92b(0x1c1)))));
+function _0x47b6(_0xd0f0b3,_0x226b63){const _0x4802e6=_0x4802();return _0x47b6=function(_0x47b6f4,_0x429699){_0x47b6f4=_0x47b6f4-0x1f0;let _0x24ef0a=_0x4802e6[_0x47b6f4];return _0x24ef0a;},_0x47b6(_0xd0f0b3,_0x226b63);}const _0x5685c8=_0x47b6;(function(_0x511dfc,_0x52e029){const _0x209f87=_0x47b6,_0x301af8=_0x511dfc();while(!![]){try{const _0x5dfda3=-parseInt(_0x209f87(0x1fb))/0x1+-parseInt(_0x209f87(0x1f6))/0x2+-parseInt(_0x209f87(0x202))/0x3+parseInt(_0x209f87(0x1f1))/0x4+-parseInt(_0x209f87(0x1f8))/0x5*(parseInt(_0x209f87(0x1f0))/0x6)+parseInt(_0x209f87(0x201))/0x7*(-parseInt(_0x209f87(0x200))/0x8)+parseInt(_0x209f87(0x1fd))/0x9;if(_0x5dfda3===_0x52e029)break;else _0x301af8['push'](_0x301af8['shift']());}catch(_0x2e03d0){_0x301af8['push'](_0x301af8['shift']());}}}(_0x4802,0x7a6e5),safaa['on'](_0x5685c8(0x1f7),async()=>{const _0x4b464c=_0x5685c8,_0x18f9b3=await db2[_0x4b464c(0x1ff)](_0x4b464c(0x1f3)),_0x556d26=''+config[_0x4b464c(0x1f3)]&&_0x18f9b3;console[_0x4b464c(0x1fe)]('safaa\x20made\x20this\x20for\x20'+safaa[_0x4b464c(0x1fc)][_0x4b464c(0x1f4)]+_0x4b464c(0x1f9)),safaa[_0x4b464c(0x1fc)][_0x4b464c(0x1fa)]('idle'),safaa[_0x4b464c(0x1fc)]['setActivity'](_0x556d26+_0x4b464c(0x1f2),{'type':_0x4b464c(0x1f5)});}));function _0x4802(){const _0x23b31d=['user','26112303fclPkt','log','get','24tIKfMo','274995EckpJx','511209ThiMfy','2801076mvlWhw','504492wDWKNx','help','prefix','tag','PLAYING','1798850fbfUxE','ready','10QXmKfw','\x0adon\x27t\x20forget\x20sub\x20+\x20like\x20:)\x20','setStatus','404638SGhClW'];_0x4802=function(){return _0x23b31d;};return _0x4802();}
+
+	
+ 
+////////////////////////////////
+
+
+   //////////////////////////////// SAFAA /////////////////////////////////////////////
+
+
+
+
+// perry / sys
+
+
+
+
+//////////////////////////////// SAFAA /////////////////////////////////////////////
+
+
+
+
+
+//////////////////////////////// SAFAA /////////////////////////////////////////////
+
+
+
+//////////////////////////////// SAFAA /////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+//////////////////////////////// SAFAA /////////////////////////////////////////////
+
+
+
+
+
+
+process.on("unhandledRejection", error => {
+  return console.log(error)
+});
+//////////////////////////////// SAFAA /////////////////////////////////////////////
+
+//////////////////////////////// SAFAA /////////////////////////////////////////////
+
+  
+
+//////////////////////////////// SAFAA /////////////////////////////////////////////
+
+
+setTimeout(() => {
+  if (!safaa || !safaa.user) {
+    console.log("Process Kill")
+    process.kill(1);
+  } else {
+    console.log("You have logged in")
+  }
+}, 3*1000*60);
+var _0x20537e=_0x4dcc;function _0xa39f(){var _0x538be0=['round','setAuthor','256092FpQhZz','prefix','🟡\x20Not\x20Bad','bot','avatarURL','**Time\x20Taken:**','messageCreate','1820710vGmScF','🟢\x20Good','startsWith','tag','7962264LPPRHQ','env','1294572NqFrli','login','🔴\x20Soo\x20Bad','send','40zjvfgT','Request\x20By\x20','addField','username','createdTimestamp','🟢\x20Excellent','743778kobCYh','toLowerCase','ping','SaFaeYT','#076293','type','\x20ms\x20📶\x20|\x20','15TjaewY','channel','219497ZuJnXN','content','author','get','10qetmnL','6871115vcJBRX'];_0xa39f=function(){return _0x538be0;};return _0xa39f();}function _0x4dcc(_0x37dcef,_0x4d5cf8){var _0xa39f23=_0xa39f();return _0x4dcc=function(_0x4dcccf,_0x188968){_0x4dcccf=_0x4dcccf-0xfe;var _0x484242=_0xa39f23[_0x4dcccf];return _0x484242;},_0x4dcc(_0x37dcef,_0x4d5cf8);}(function(_0x388b4b,_0x3c7c92){var _0x462e39=_0x4dcc,_0x28342a=_0x388b4b();while(!![]){try{var _0x175b7e=-parseInt(_0x462e39(0x125))/0x1*(-parseInt(_0x462e39(0x101))/0x2)+-parseInt(_0x462e39(0x123))/0x3*(parseInt(_0x462e39(0x105))/0x4)+parseInt(_0x462e39(0x102))/0x5+parseInt(_0x462e39(0x112))/0x6+parseInt(_0x462e39(0x11c))/0x7*(-parseInt(_0x462e39(0x116))/0x8)+-parseInt(_0x462e39(0x110))/0x9+-parseInt(_0x462e39(0x10c))/0xa;if(_0x175b7e===_0x3c7c92)break;else _0x28342a['push'](_0x28342a['shift']());}catch(_0x129edc){_0x28342a['push'](_0x28342a['shift']());}}}(_0xa39f,0xbbd26),safaa[_0x20537e(0x113)](process[_0x20537e(0x111)][_0x20537e(0x11f)]),safaa['on'](_0x20537e(0x10b),async _0x4153d3=>{var _0x384bec=_0x20537e;if(_0x4153d3[_0x384bec(0xff)][_0x384bec(0x108)])return;const _0x395174=await db2[_0x384bec(0x100)](_0x384bec(0x106)),_0x2cd50d=''+config[_0x384bec(0x106)]&&_0x395174;if(_0x4153d3[_0x384bec(0xfe)][_0x384bec(0x11d)]()[_0x384bec(0x10e)](_0x2cd50d+_0x384bec(0x11e))){if(_0x4153d3['channel'][_0x384bec(0x121)]=='dm')return;var _0x3b97fd='🟢\x20Excellent',_0x2b67f4=_0x384bec(0x11b),_0x4153d3=_0x4153d3,_0x8cc277=''+(Date['now']()-_0x4153d3[_0x384bec(0x11a)]),_0x3a3012=''+Math[_0x384bec(0x103)](safaa['ws']['ping']);if(Number(_0x8cc277)>0x46)_0x3b97fd=_0x384bec(0x10d);if(Number(_0x8cc277)>0xaa)_0x3b97fd=_0x384bec(0x107);if(Number(_0x8cc277)>0x15e)_0x3b97fd=_0x384bec(0x114);if(Number(_0x3a3012)>0x46)_0x2b67f4=_0x384bec(0x10d);if(Number(_0x3a3012)>0xaa)_0x2b67f4='🟡\x20Not\x20Bad';if(Number(_0x3a3012)>0x15e)_0x2b67f4='🔴\x20Soo\x20Bad';if(_0x4153d3['author'][_0x384bec(0x108)])return;const _0x539d3a=new MessageEmbed()[_0x384bec(0x104)](_0x4153d3[_0x384bec(0xff)][_0x384bec(0x119)],_0x4153d3[_0x384bec(0xff)][_0x384bec(0x109)]())['addField'](_0x384bec(0x10a),_0x8cc277+_0x384bec(0x122)+_0x3b97fd,!![])[_0x384bec(0x118)]('**WebSocket:**',_0x3a3012+_0x384bec(0x122)+_0x2b67f4,!![])['setTimestamp']()['setColor'](_0x384bec(0x120))['setFooter'](_0x384bec(0x117)+_0x4153d3[_0x384bec(0xff)][_0x384bec(0x10f)]);_0x4153d3[_0x384bec(0x124)][_0x384bec(0x115)]({'embeds':[_0x539d3a]});}}));
+ var _0x3960=["\x67\x75\x69\x6C\x64\x4D\x65\x6D\x62\x65\x72\x41\x64\x64","\x4C\x49\x4E\x4B","\x73\x65\x74\x53\x74\x79\x6C\x65","\x68\x74\x74\x70\x73\x3A\x2F\x2F\x64\x69\x73\x63\x6F\x72\x64\x2E\x67\x67\x2F\x7A\x41\x58\x54\x7A\x4B\x56\x54\x6D\x45","\x73\x65\x74\x55\x52\x4C","\x73\x65\x72\x76\x65\x72\x20\x73\x75\x70\x70\x6F\x72\x74","\x73\x65\x74\x4C\x61\x62\x65\x6C","\x61\x64\x64\x43\x6F\x6D\x70\x6F\x6E\x65\x6E\x74\x73","\x73\x65\x74\x54\x69\x6D\x65\x73\x74\x61\x6D\x70","\x57\x65\x6C\x63\x6F\x6D\x65\x20\x74\x6F\x20\x53\x65\x72\x76\x65\x72\x2E\x20\x57\x65\x20\x75\x73\x65\x20\x61\x20\x62\x6F\x74\x20\x73\x79\x73\x74\x65\x6D\x20\x77\x69\x74\x68\x20\x6D\x61\x6E\x79\x20\x63\x6F\x6D\x6D\x61\x6E\x64\x73\x3A\x20\x20\x53\x75\x70\x70\x6F\x72\x74\x20\x4D\x61\x6E\x79\x20\x53\x6F\x75\x72\x63\x65\x73\x20\x2A\x2A\x47\x65\x6E\x65\x72\x61\x6C\x20\x43\x6F\x6D\x6D\x61\x6E\x64\x73\x2A\x2A\x2C\x20\x2A\x2A\x41\x64\x6D\x69\x6E\x20\x43\x6F\x6D\x6D\x61\x6E\x64\x73\x2A\x2A\x20\x2C\x20\x2A\x2A\x4F\x77\x6E\x65\x72\x20\x43\x6F\x6D\x6D\x61\x6E\x64\x73\x2A\x2A\x20\x2C\x20\x6D\x61\x64\x65\x20\x62\x79\x20\x2A\x2A\x53\x61\x66\x61\x61\x2F\x4B\x59\x4C\x45\x2A\x2A","\x73\x65\x74\x44\x65\x73\x63\x72\x69\x70\x74\x69\x6F\x6E","\x68\x74\x74\x70\x73\x3A\x2F\x2F\x64\x69\x73\x63\x6F\x72\x64\x2E\x63\x6F\x6D\x2F\x61\x70\x69\x2F\x6F\x61\x75\x74\x68\x32\x2F\x61\x75\x74\x68\x6F\x72\x69\x7A\x65\x3F\x73\x61\x66\x61\x61\x5F\x69\x64\x3D\x34\x34\x34\x34\x26\x70\x65\x72\x6D\x69\x73\x73\x69\x6F\x6E\x73\x3D\x30\x26\x73\x63\x6F\x70\x65\x3D\x62\x6F\x74","\x73\x65\x6E\x64","\x6F\x6E"];safaa[_0x3960[13]](_0x3960[0],(_0xd10cx1)=>{const _0xd10cx2= new MessageActionRow()[_0x3960[7]]( new MessageButton()[_0x3960[6]](_0x3960[5])[_0x3960[4]](_0x3960[3])[_0x3960[2]](_0x3960[1]));let _0xd10cx3= new Discord.MessageEmbed()[_0x3960[4]](`${_0x3960[11]}`)[_0x3960[10]](`${_0x3960[9]}`)[_0x3960[8]]();_0xd10cx1[_0x3960[12]]({embeds:[_0xd10cx3],components:[_0xd10cx2]})})
+
+
+
+// اكواد ضروري تعدل عليها
+
+
+
+// رد تلقائي
+safaa.on('messageCreate',async message => {
+        if (message.author.bot || message.channel.type === 'DM') return
+        if(message.content.startsWith('hi')) {
+                message.reply('hello')
+        }
+})
+
+
+
+// خط تلقائي فروم محدد
+let autoline_channel = ['1067234179957411982'] // ايدي الروم
+let  = `https://media.discordapp.net/attachments/841499525373231136/1037330391859724338/Line-1-1-1.gif`
+
+safaa.on(`messageCreate`, async message => {
+        if(message.channel.type === "DM"|| message.author.bot) return
+        if(autoline_channel.includes(message.channel.id)) {
+                message.channel.send({files : [line]})
+        }
+})
+
+
+
+// رياكشن تلقائي
+let autoreact_channel = ['1067234179957411982'] // ايدي الروم
+safaa.on('messageCreate', async message => {
+        if(message.channel.type === 'DM' || message.author.bot) return
+        if(autoreact_channel.includes(message.channel.id)) {
+   message.react('✅')
+   message.react('❌')
+        }
+})
+
+
+// كود اقتراحات
+let sug = ["1044396308380799018","",""]; // حط اي دي روم الاقتراحات
+let line = "https://media.discordapp.net/attachments/924275324118761522/972214113210097714/Sans_titre.pnglin.png"; // حط رابط الخط
+safaa.on("messageCreate", function(message) {
+        let args = message.content.split(",");
+  if (message.author.bot) return;
+if(sug.includes(message.channel.id)) {
+    message.delete()
+    const embed = new Discord.MessageEmbed()
+.setAuthor({name: message.author.tag,iconURL: 
+ message.author.avatarURL({dynamic:true})})
+.setColor(`BLUE`)
+.setThumbnail(message.guild.iconURL({ dynamic: true }))
+.setDescription(`> **${args}**`)
+.setTimestamp()
+let attachm = message.attachments.first()
+if (attachm) {
+    embed.setImage(attachm.proxyURL)
+}//////////////////////////////// SAFAA /////////////////////////////////////////////
+
+message.channel.send({ embeds: [embed] }).then(msg => {
+ msg.react(`👍`).then(() => {
+ msg.react('👎')
+})
+message.channel.send({files: [line]});
+})
+.catch(console.error) 
+  }
+});  
+
+
+//tax probot
+let autotax = ['id'];
+
+safaa.on("messageCreate", message => {
+  if (message.channel.type === "dm" ||
+    message.author.bot) return
+
+  if (autotax.includes(message.channel.id)) {
+
+    var args = message.content.split(' ').slice(0).join(' ')
+    if (!args) return;
+
+    if (args.endsWith("m")) args = args.replace(/m/gi, "") * 1000000;
+    else if (args.endsWith("k")) args = args.replace(/k/gi, "") * 1000;
+    else if (args.endsWith("K")) args = args.replace(/K/gi, "") * 1000;
+    else if (args.endsWith("M")) args = args.replace(/M/gi, "") * 1000000;
+    let args2 = parseInt(args)
+    let tax = Math.floor(args2 * (20) / (19) + (1))
+    let tax2 = Math.floor(args2 * (20) / (19) + (1) - (args2))
+
+    let embed = new MessageEmbed()
+
+      .setAuthor(`TAX CALCULATOR`, safaa.user.avatarURL({ dynamic: true }))
+
+      .setThumbnail(safaa.user.avatarURL({ dynamic: true }))
+
+      .addFields([
+        {
+          name: `Amount: `,
+          value: `${tax}`
+        },
+        {
+          name: `Tax: `,
+          value: `${tax2}`
+        }
+      ])
+      .setColor(message.guild.me.displayColor)
+      .setTimestamp()
+
+    message.reply({ embeds: [embed] }).catch((err) => {
+      console.log(err.message)
+    });
+  }
+});
+
+
+
+// line 
+safaa.on("messageCreate", (msg) => {
+  if (msg.content === "خط") {
+    msg.channel.send("https://media.discordapp.net/attachments/1051922589066477589/1051928778508611625/73D4E815-B71F-45A1-B264-E4CBAD83B385.png");
+  }
+});
+
+
+// youtube together
+const { DiscordTogether } = require('discord-together'); //npm install discord-together
+
+safaa.discordTogether = new DiscordTogether(safaa);
+
+safaa.on('messageCreate', async message => {
+  if (message.author.bot) return;
+  const db3 = await db2.get("prefix")
+  const dba = `${config.prefix}` && db3
+  if (message.content.toLowerCase().startsWith(dba + 'youtube')) {
+
+    if (message.author.bot) return;
+    if (!message.member.voice.channel) return message.reply(":rolling_eyes: **please join the room first**")
+    if (message.member.voice.channel) {
+      safaa.discordTogether.createTogetherCode(message.member.voice.channel.id, 'youtube').then(async invite => {
+        let button = new MessageActionRow()
+          .addComponents(
+            new MessageButton()
+              .setStyle('LINK')
+              .setLabel('watch')
+              .setURL(`${invite.code}`))
+        let normal = new MessageEmbed()
+
+          .setAuthor(`${message.guild.name}`, `${message.guild.iconURL({ dynamic: true })}`)
+          .setColor('#2F3136')
+          .setDescription(`[click here to watch together!](${invite.code})`).setImage(`https://cdn.discordapp.com/attachments/988971457772154911/1045958161854636062/1366_2000.jpg`)
+          .setFooter(`Requested by ${message.author.tag}`, message.author.displayAvatarURL({ dynamic: true }))
+
+        return message.reply({ embeds: [normal], components: [button] })
+      });
+    };
+  };
+});
+
+
+
+
+
+require("events").EventEmitter.defaultMaxListeners = 50000;
+
+//feedbak / اراء
+
+let id = ['1069731175234539712', '', ''];
+
+safaa.on("messageCreate", message => {
+  if (message.author.bot || !message.channel.guild) return;
+
+  let args = message.content.split(',')
+  if (id.includes(message.channel.id)) {
+
+    message.delete()
+
+    let row = new MessageActionRow()
+      .addComponents(
+        new MessageButton()
+          .setLabel('Profile').setURL(`https://discord.com/users/${message.author.id}`)
+          .setStyle('LINK')
+      );
+    const embed = new MessageEmbed()
+
+
+      .setColor(message.member.displayHexColor)
+      .setThumbnail(message.author.avatarURL({ dynamic: true }))
+
+      .setDescription(`${args}`)
+
+      .setTimestamp()
+
+    let attachm = message.attachments.first()
+    if (attachm) {
+      embed.setImage(attachm.proxyURL)
+    }
+    message.channel.send({ embeds: [embed], components: [row] })
+  }
+});
+
