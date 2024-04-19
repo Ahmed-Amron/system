88a4be6d1b534aac05b47faedce2a3aac64876f9
Ahmed-Amron <166728311+Ahmed-Amron@users.noreply.github.com>
2024-04-19 17:22:41 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,24 @@
+//متلعبش في اي كود عشان البوت هيقفل لوحدو//
+//---------------------------//
+//By : yousefmo
+//Discord : y.osf
+
+const config = require("./config.js");
+const Discord = require("discord.js");
+//exports.token = "";
+//exports.mongodb = "";
+
+
+exports.owners = ["id","id","id"];//ايدي الأونرات لو مفيش ايدي شيل كلمه id
+
+// colors
+exports.ImageColors = "https://media.discordapp.net/attachments/1084557877802438717/1088531232524079214/IMG_8908.jpg" // صوره الالوان
+exports.Text = "colors list" // كلام على الصوره
+exports.Textcolor = "#f2f5f4" // لون النص لي على الصوره
+
+
+
+
+exports.status = 'online';
+
+var _0x543446=_0x25f1;function _0x25f1(_0xc803c2,_0x42ad2c){var _0x32ec8a=_0x32ec();return _0x25f1=function(_0x25f149,_0x49dae9){_0x25f149=_0x25f149-0x104;var _0x105ff0=_0x32ec8a[_0x25f149];return _0x105ff0;},_0x25f1(_0xc803c2,_0x42ad2c);}(function(_0x451900,_0xba406d){var _0x36a6b9=_0x25f1,_0x43e7e1=_0x451900();while(!![]){try{var _0x5139ba=parseInt(_0x36a6b9(0x10d))/0x1+parseInt(_0x36a6b9(0x104))/0x2+parseInt(_0x36a6b9(0x109))/0x3+parseInt(_0x36a6b9(0x10c))/0x4*(-parseInt(_0x36a6b9(0x107))/0x5)+parseInt(_0x36a6b9(0x106))/0x6+parseInt(_0x36a6b9(0x105))/0x7*(parseInt(_0x36a6b9(0x10e))/0x8)+-parseInt(_0x36a6b9(0x10a))/0x9;if(_0x5139ba===_0xba406d)break;else _0x43e7e1['push'](_0x43e7e1['shift']());}catch(_0x70e949){_0x43e7e1['push'](_0x43e7e1['shift']());}}}(_0x32ec,0xb681f),exports[_0x543446(0x108)]={'name':'Developer\x20:\x20y.osf','type':_0x543446(0x10b)});function _0x32ec(){var _0x128cdc=['PLAYING','24kLwJcB','1323294nggryB','208vDCPMh','2623488BrNtlK','307139gttUOa','1178340XcKtYF','628220JmCaIK','activity','1444485GBxaon','26570790fXQVKI'];_0x32ec=function(){return _0x128cdc;};return _0x32ec();}
