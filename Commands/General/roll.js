3d06bf0ea903928d04f09a74faa7f29291a5b5b2
Ahmed-Amron <166728311+Ahmed-Amron@users.noreply.github.com>
2024-04-20 06:44:30 +0200

    Delete Commands directory

--- Commands/General/roll.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'roll', // هنا اسم الامر
-    run : (client, message, args) => {
-        
-        const randomNumber = Math.floor(Math.random() * 100) + 1;
-    message.reply(`🎲**${randomNumber}**`); 
-
-    }
-}
